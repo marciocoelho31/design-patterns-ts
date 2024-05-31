fe451dad56f05f241de52447a3874739024c3efc
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2024-05-31 10:14:16 -0300

    Implementando padrão de projeto Bridge

---
+++ src/structural/bridge/main.ts
@@ -0,0 +1,25 @@
+import { Radio } from './device/radio';
+import { Tv } from './device/tv';
+import { RemoteControl } from './remote-control/remote-control';
+import { RemoteControlWithVolume } from './remote-control/remote-control-with-volume';
+
+export function clientCode(
+  abstraction: RemoteControl | RemoteControlWithVolume,
+): void {
+  abstraction.togglePower();
+
+  // Type Guard
+  if (!('volumeUp' in abstraction)) return;
+
+  abstraction.volumeUp();
+  abstraction.volumeUp();
+  abstraction.volumeDown();
+}
+
+const radio = new Radio();
+const radioRemoteControl = new RemoteControl(radio);
+clientCode(radioRemoteControl);
+
+const tv = new Tv();
+const tvRemoteControl = new RemoteControlWithVolume(tv);
+clientCode(tvRemoteControl);
