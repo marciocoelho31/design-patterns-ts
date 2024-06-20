9ac680a4dd27dd1016cc724def3764859551c2f5
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2024-06-20 16:27:35 -0300

    Implementando padrão de projeto Mediator

---
+++ src/behavioural/mediator/main.ts
@@ -0,0 +1,22 @@
+import { Buyer } from './buyer';
+import { Mediator } from './mediator';
+import { Seller } from './seller';
+
+const mediator = new Mediator();
+
+const seller1 = new Seller();
+seller1.addProduct({ id: '1', name: 'Camiseta', price: 49.9 });
+seller1.addProduct({ id: '2', name: 'Calça jeans', price: 129.9 });
+
+const seller2 = new Seller();
+seller2.addProduct({ id: '3', name: 'Gravata', price: 79.9 });
+seller2.addProduct({ id: '4', name: 'Sapato', price: 329.9 });
+
+mediator.addSeller(seller1, seller2);
+//mediator.showProducts();
+
+const buyer = new Buyer(mediator);
+buyer.viewProducts();
+buyer.buy('2');
+buyer.buy('3');
+buyer.viewProducts();
---
+++ src/behavioural/mediator/seller.ts
@@ -0,0 +1,33 @@
+import { Mediator } from './mediator';
+import { SellerProduct } from './seller-product';
+
+export class Seller {
+  private products: SellerProduct[] = [];
+  private mediator?: Mediator;
+
+  showProducts(): void {
+    this.products.forEach((product) => {
+      console.log(product.id, product.name, product.price);
+    });
+  }
+
+  addProduct(...products: SellerProduct[]): void {
+    this.products.push(...products);
+  }
+
+  setMediator(mediator: Mediator): void {
+    this.mediator = mediator;
+  }
+
+  sell(id: string): SellerProduct | void {
+    const productIndex = this.products.findIndex(
+      (product) => product.id === id,
+    );
+    if (productIndex === -1) {
+      return;
+    }
+
+    const product = this.products.splice(productIndex, 1);
+    return product[0];
+  }
+}
